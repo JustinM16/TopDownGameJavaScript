66aec5c3da447e1829a575c002b035c9d683aff3
Justin Mendes <Justin.Mendes16@gmail.com>
2017-02-15 02:05:22 -0500

    Update TableSetup.js

--- Scripts/TableSetup.js
+++ Scripts/TableSetup.js
@@ -10,24 +10,24 @@ var Scores = [10];
 
 
 
-for (i = 0; i < 10; i++){
-	Scores[i] = {
-	Pos: i+1,
-	Name: "Justin",
-	Kills: "Billions"};
-}
+//for (i = 0; i < 10; i++){
+//	Scores[i] = {
+//	Pos: i+1,
+//	Name: "Justin",
+//	Kills: "Billions"};
+//}
 
-for (i = 0; i < 10; i++){
-	row = table.insertRow(i+1);
-	posCell = row.insertCell(0);
-	UserCell = row.insertCell(1);
-	KillCell = row.insertCell(2);
-
-	for (k = 0; k<30; k++){
-		posCell.innerHTML = Scores[i].Pos;
-		UserCell.innerHTML = Scores[i].Name;
-		KillCell.innerHTML = Scores[i].Kills;
-		k = k+3;
-	}
-
-}
+//for (i = 0; i < 10; i++){
+//	row = table.insertRow(i+1);
+//	posCell = row.insertCell(0);
+//	UserCell = row.insertCell(1);
+//	KillCell = row.insertCell(2);
+//
+//	for (k = 0; k<30; k++){
+//		posCell.innerHTML = Scores[i].Pos;
+//		UserCell.innerHTML = Scores[i].Name;
+//		KillCell.innerHTML = Scores[i].Kills;
+//		k = k+3;
+//	}
+//
+//}
